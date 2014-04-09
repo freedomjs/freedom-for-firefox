751d01ceba3c9ebb9b1f6d1d358103f7117ebb23
Paul Ellenbogen <epaul9@cs.washington.edu>
2014-04-09 11:22:42 -0700

    Add initial files.

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+var FILES = require('freedom/Gruntfile.js').FILES;
+
+for (var key in FILES) {
+  FILES[key] = FILES[key].map(function(str) {
+    if (str[0] === '!') {
+      return '!node_modules/freedom/' + str.substr(1);
+    } else {
+      return 'node_modules/freedom/' + str;
+    }
+  });
+}
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      providers: ['providers/*.js'],
+      options: {
+        '-W069': true,
+        '-W104': false
+      }
+    },
+    concat: {
+      dist: {
+        options: {
+          process: function(src) {
+            return src.replace(/\/\*jslint/g,'/*');
+          }
+        },
+        src: ['src/firefox-preamble.js'] 
+            .concat(FILES.preamble)
+            .concat(FILES.src)
+            //.concat('providers/*.js')
+            .concat(FILES.postamble)
+            .concat('src/firefox-postamble.js'),
+        dest: 'freedom-for-firefox.jsm'
+      }
+    },
+    clean: ["freedom-for-firefox.jsm"]
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.registerTask('freedom-firefox', [
+    'jshint:providers',
+    'concat'
+  ]);
+  grunt.registerTask('default', ['freedom-firefox']);
+};
+
