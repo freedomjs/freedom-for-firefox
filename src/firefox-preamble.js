ae17fc2b25f4661df2242ec50d13daea9194c7c2
Paul Ellenbogen <epaul9@cs.washington.edu>
2014-05-05 08:46:27 -0700

    Add globals for WebRTC.

--- src/firefox-preamble.js
+++ src/firefox-preamble.js
@@ -1,9 +1,15 @@
 Components.utils.import("resource://gre/modules/devtools/Console.jsm");
 Components.utils.import("resource://gre/modules/Timer.jsm");
+Components.utils.import('resource://gre/modules/Services.jsm');
 // This module does not support all of es6 promise functionality.
 // Components.utils.import("resource://gre/modules/Promise.jsm");
 const XMLHttpRequest = Components.Constructor("@mozilla.org/xmlextras/xmlhttprequest;1", "nsIXMLHttpRequest");
 Components.utils.importGlobalProperties(['URL']);
+
+var hiddenWindow = Services.appShell.hiddenDOMWindow;
+var mozRTCPeerConnection = hiddenWindow.mozRTCPeerConnection;
+var mozRTCSessionDescription = hiddenWindow.mozRTCSessionDescription;
+var mozRTCIceCandidate = hiddenWindow.mozRTCIceCandidate;
 
 var freedom;
 
