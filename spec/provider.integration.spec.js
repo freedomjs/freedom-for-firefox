c77e582c52ee6599a5d10620105f791f0f17283d
soycode <soycode@users.noreply.github.com>
2016-03-07 16:32:51 -0800

    switch to using grunt-jasmine-firefoxaddon for tests

--- spec/provider.integration.spec.js
+++ spec/provider.integration.spec.js
@@ -21,50 +21,50 @@ XPCOMUtils.importRelative(jsmContext, "freedom-for-firefox.jsm");
 
 // Social
 describe("integration-single: social.loopback.json", require("freedom/spec/providers/social/social.single.integration.src")
-  .bind(this, freedom, base+"/providers/social/loopback/social.loopback.json"), {});
+         .bind(this, freedom, base+"/providers/social/loopback/social.loopback.json", {}));
 describe("integration-single: social.ws.json", require("freedom/spec/providers/social/social.single.integration.src")
-  .bind(this, freedom, base+"/providers/social/websocket-server/social.ws.json", {}));
+         .bind(this, freedom, base+"/providers/social/websocket-server/social.ws.json", {}));
 describe("integration-double: social.ws.json", require("freedom/spec/providers/social/social.double.integration.src")
-  .bind(this, freedom, base+"/providers/social/websocket-server/social.ws.json", {}));
+         .bind(this, freedom, base+"/providers/social/websocket-server/social.ws.json", {}));
 
 // Storage
 describe("integration: storage.isolated.json", require("freedom/spec/providers/storage/storage.integration.src")
-  .bind(this, freedom, base+"/providers/storage/isolated/storage.isolated.json", {}, false));
+         .bind(this, freedom, base+"/providers/storage/isolated/storage.isolated.json", {}, false));
 describe("integration: storage.shared.json", require("freedom/spec/providers/storage/storage.integration.src")
-  .bind(this, freedom, base+"/providers/storage/shared/storage.shared.json", {}, false));
+         .bind(this, freedom, base+"/providers/storage/shared/storage.shared.json", {}, false));
 
 // Transport
 describe("integration: transport.webrtc.json",
-    require('freedom/spec/providers/transport/transport.integration.src').bind(this,
-    "providers/transport/webrtc/transport.webrtc.json", setup));
+         require('freedom/spec/providers/transport/transport.integration.src').bind(this,
+                                                                                    "providers/transport/webrtc/transport.webrtc.json", setup));
 
 describe("integration: core.rtcpeerconnection",
-    require('freedom/spec/providers/coreIntegration/rtcpeerconnection.integration.src').bind(this,
-    require("freedom/providers/core/core.rtcpeerconnection"),
-    require("freedom/providers/core/core.rtcdatachannel"),
-    setup));
+         require('freedom/spec/providers/coreIntegration/rtcpeerconnection.integration.src').bind(this,
+                                                                                                  require("freedom/providers/core/core.rtcpeerconnection"),
+                                                                                                  require("freedom/providers/core/core.rtcdatachannel"),
+                                                                                                  setup));
 
 describe("integration: core.xhr",
-    require("freedom/spec/providers/coreIntegration/xhr.integration.src").bind(this,
-    require("freedom/providers/core/core.xhr"), setup));
+         require("freedom/spec/providers/coreIntegration/xhr.integration.src").bind(this,
+                                                                                    require("freedom/providers/core/core.xhr"), setup));
 
 describe("integration: core.tcpsocket",
          require('freedom/spec/providers/coreIntegration/tcpsocket.integration.src').bind(this,
                                                                                           require('../providers/core.tcpsocket'), setup));
 
 describe("integration: core.udpsocket",
-    require('freedom/spec/providers/coreIntegration/udpsocket.integration.src').bind(this,
-    require('../providers/core.udpsocket'), setup));
+         require('freedom/spec/providers/coreIntegration/udpsocket.integration.src').bind(this,
+                                                                                          require('../providers/core.udpsocket'), setup));
 
 describe("integration: core.oauth - progresslistener",
-    require("freedom/spec/providers/coreIntegration/oauth.integration.src").bind(this,
-    require("freedom/providers/core/core.oauth"),
-    [ require("../providers/oauth/oauth.progresslistener") ],
-    [ "https://localhost:9876/" ],
-    setup));
+         require("freedom/spec/providers/coreIntegration/oauth.integration.src").bind(this,
+                                                                                      require("freedom/providers/core/core.oauth"),
+                                                                                      [ require("../providers/oauth/oauth.progresslistener") ],
+                                                                                      [ "https://localhost:9876/" ],
+                                                                                      setup));
 describe("integration: core.oauth - httpobserver",
-    require("freedom/spec/providers/coreIntegration/oauth.integration.src").bind(this,
-    require("freedom/providers/core/core.oauth"),
-    [ require("../providers/oauth/oauth.httpobserver") ],
-    [ "https://localhost:9876/" ],
-    setup));
+         require("freedom/spec/providers/coreIntegration/oauth.integration.src").bind(this,
+                                                                                      require("freedom/providers/core/core.oauth"),
+                                                                                      [ require("../providers/oauth/oauth.httpobserver") ],
+                                                                                      [ "https://localhost:9876/" ],
+                                                                                      setup));
